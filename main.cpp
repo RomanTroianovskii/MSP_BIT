f8b22860f9519b2543624770722f736c3e5e6210
🌴 ._ṛσꪔάṛīσ_.🌴 <154616284+RomanTroianovskii@users.noreply.github.com>
2024-10-12 22:45:51 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,71 @@
-// Bits.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// MSHP_SB_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+#define __end_s "\n"
 #include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;
+    set<int> c1, c2;
+    vector<int> colors, v1, v2;
 
-int main()
-{
-        int n, k;
-        std::cin >> n >> k;
-        std::cout << (1 << k) << std::endl;
-        std::cout << ((n >> k) & 1) << std::endl;
-        std::cout << ((n >> k) << k) << std::endl;
-        if (((n >> k) & 1) == 0) {
-            std::cout << n << std::endl;
+
+    for (int i = 0; i < n; ++i) {
+        int color;
+        cin >> color;
+        c1.insert(color);
+    }
+
+
+    for (int i = 0; i < m; ++i) {
+        int color;
+        cin >> color;
+        c2.insert(color);
+    }
+
+    for (const int& color : c1) {
+        if (c2.count(color)) {
+            colors.push_back(color);
         }
         else {
-            std::cout << n - (1 << k) << std::endl;
+            v1.push_back(color);
         }
-        if (((n >> k) & 1) == 1) {
-            std::cout << n - (1 << k);
+    }
+
+    for (const int& color : c2) {
+        if (!c1.count(color)) {
+            v2.push_back(color);
         }
-        else {
-            std::cout << n + (1 << k);
-        }
- 
+    }
+
+
+    cout << colors.size() << __end_s;
+
+
+    for (const int& color : colors) {
+        cout << color << " ";
+    }
+    cout << __end_s;
+
+
+
+    cout << v1.size() << __end_s;
+    for (const int& color : v1) {
+        cout << color << " ";
+    }
+    cout << __end_s;
+
+
+    cout << v2.size() << __end_s;
+    for (const int& color : v2) {
+        cout << color << " ";
+    }
+    cout << __end_s;
+
+
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
